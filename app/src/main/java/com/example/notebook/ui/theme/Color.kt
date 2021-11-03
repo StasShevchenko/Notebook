384d90703f23e1948f6c81dbb0e9df1818a5960c
StasShevchenko <dark.knight22832@gmail.com>
2021-11-03 14:13:06 +0300

    Initial commit with organized package structure

---
+++ app/src/main/java/com/example/notebook/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.notebook.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val LightBlue = Color(0xFFD7E8DE)
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
