c33b3e58398777ad8e12dec493c8cc9511292268
Стас Шевченко <dark.knight22832@gmail.com>
2022-01-05 15:55:17 +0300

    Delete use case is created; Wrapper use cases class is created

---
+++ app/src/main/java/com/example/notebook/feature_notebook/domain/use_case/DeleteEntry.kt
@@ -0,0 +1,13 @@
+package com.example.notebook.feature_notebook.domain.use_case
+
+import com.example.notebook.feature_notebook.domain.model.PeopleInfo
+import com.example.notebook.feature_notebook.domain.repository.NotebookRepository
+
+class DeleteEntry(
+    private val repository: NotebookRepository
+) {
+
+    suspend operator fun invoke(entryId: Int){
+        repository.deleteEntry(entryId)
+    }
+}
---
+++ app/src/main/java/com/example/notebook/feature_notebook/domain/use_case/NotebookUseCases.kt
@@ -0,0 +1,6 @@
+package com.example.notebook.feature_notebook.domain.use_case
+
+data class NotebookUseCases(
+    val getEntries: GetEntries,
+    val deleteEntry: DeleteEntry
+)
